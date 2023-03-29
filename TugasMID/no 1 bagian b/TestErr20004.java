9aed691c2a3b5d7a523455eaadaa95dc25c87837
aisyar94 <127504299+aisyar94@users.noreply.github.com>
2023-03-28 22:51:46 -0700

    Create TestErr20004.java

---
+++ TugasMID/no 1 bagian b/TestErr20004.java
@@ -0,0 +1,23 @@
+/* Aisyah Safira Rachman */
+/* 13020210004 */
+
+public class TestErr20004 {
+    public static void main(String[] args) {
+        int tot = 0;
+        int j = 0;
+        for (int i = 0; i < 10; i++) {
+            tot += i;
+            if (i < j) {
+                System.out.println(i);
+            } else {
+                System.out.println(j);
+            }
+        }
+        do {
+            j++;
+        } while (j < 10);
+        while (j < 10) {
+            j++;
+        }
+    }
+}
